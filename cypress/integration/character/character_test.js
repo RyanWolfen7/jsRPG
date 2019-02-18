0cc45e35675de7953c0cbd9d605949ba20ee623c
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-18 23:18:13 +0000

    added stats test to character

--- cypress/integration/character/character_test.js
+++ cypress/integration/character/character_test.js
@@ -2,12 +2,20 @@ import Character from '../../../models/characters/character.js';
 
 describe(Character, function() {
   let character = new Character("Name", "Race", "Class")
+  let defaultStats = {
+    str: 12, dex: 12, const: 12,
+    char: 12, wis: 12, int: 12
+  }
 
   describe("Constructor", function() {
     it("Has Name, Race, and Class", function() {
       expect(character.name).to.eql("Name");
       expect(character.race).to.eql("Race");
       expect(character.cClass).to.eql("Class");
+    })
+
+    it("Should have default stats", function() {
+      expect(character.stats).to.eql(defaultStats);
     })
   })
 })
