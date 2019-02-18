d35a125d1c5e51080099395530e340c43077d7f7
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-18 23:38:18 +0000

    got rid of level magic number

--- models/characters/character.js
+++ models/characters/character.js
@@ -3,12 +3,15 @@ const DEFAULT_STATS = {
   char: 12, wis: 12, int: 12
 };
 
+const DEFAULT_LEVEL = 1;
+
 class Character {
   constructor(name, race, cClass, stats = DEFAULT_STATS) {
     this.name   = name,
     this.race   = race,
     this.cClass = cClass,
-    this.stats  = stats
+    this.stats  = stats,
+    this.level = DEFAULT_LEVEL
   }
 
 }
