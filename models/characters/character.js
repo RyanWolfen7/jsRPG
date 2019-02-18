5ce8927ddb640d500864bc6973a696ca1d828191
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-18 23:18:36 +0000

    added stats to character

--- models/characters/character.js
+++ models/characters/character.js
@@ -1,8 +1,14 @@
+const DEFAULT_STATS = {
+  str: 12, dex: 12, const: 12,
+  char: 12, wis: 12, int: 12
+};
+
 class Character {
-  constructor(name, race, cClass) {
+  constructor(name, race, cClass, stats = DEFAULT_STATS) {
     this.name = name,
     this.race = race,
-    this.cClass = cClass
+    this.cClass = cClass,
+    this.stats = stats
   }
 
 }
