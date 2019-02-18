fd02b917797fc3304dd6542ef55c9b7798caf86a
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-18 23:02:41 +0000

    added name test

--- cypress/integration/character/character_test.js
+++ cypress/integration/character/character_test.js
@@ -2,4 +2,10 @@ import Character from '../../../models/characters/character.js';
 
 describe(Character, function() {
   let character = new Character("Name", "Race", "Class")
+
+  describe("Constructor", function() {
+    it("Has Name, Race, and Class", function() {
+      expect(character.name).to.eql("Name")
+    })
+  })
 })
