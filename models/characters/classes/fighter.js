62594e6bd86de1e21a02e86efb3019a7ce4bda4d
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-20 01:56:57 +0000

    refactored fighter class

--- models/characters/classes/fighter.js
+++ models/characters/classes/fighter.js
@@ -1,14 +1,16 @@
 import CharClass from '../class.js'
 
+const BONUS =  {
+  str: 1, dex: 1, const:1,
+  char: 0, wis: 0, int: 0
+};
+
 class Fighter extends CharClass {
   constructor(props) {
     super(props);
 
     this.name = "Fighter",
-    this.bonus = {
-      str: 1, dex: 1, const:1,
-      char: 0, wis: 0, int: 0 
-     }
+    this.bonus = BONUS
   }
 }
 
