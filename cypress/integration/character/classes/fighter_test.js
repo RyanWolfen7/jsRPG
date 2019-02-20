0397afdb98370cb9af08d3ca1e3f7e8fd63589ed
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-20 00:49:58 +0000

    removed class arch class and added fighter tests

--- cypress/integration/character/classes/fighter_test.js
+++ cypress/integration/character/classes/fighter_test.js
@@ -1,1 +1,20 @@
 import Fighter from '../../../../models/characters/classes/fighter.js';
+
+describe('Fighter Class', function() {
+  let fighter = new Fighter('skill');
+  let bonus = { str: 1, dex: 1, const:1 };
+
+  describe('constructor', function() {
+    it('should have the name fighter', function() {
+      expect(fighter.name).to.eql("Fighter");
+    })
+
+    it('should have + 1 on str, dex, const', function() {
+      expect(fighter.bonus).to.eql(bonus);
+    })
+
+    it('should have the skill string', function() {
+      expect(fighter.skills).to.eql('skill')
+    })
+  })
+})
