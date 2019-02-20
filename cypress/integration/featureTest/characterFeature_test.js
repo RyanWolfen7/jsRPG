12a52291c80a2cbf300cce0cf8428d5c1f232062
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-20 01:14:09 +0000

    added check test to see if class bonus is added

--- cypress/integration/featureTest/characterFeature_test.js
+++ cypress/integration/featureTest/characterFeature_test.js
@@ -2,17 +2,27 @@ import Character from '../../../models/characters/character.js';
 import Fighter from '../../../models/characters/classes/fighter.js';
 
 describe('Character load test', function() {
-  let fighter = new Fighter('skills');
-  let character = new Character("Ryan", "Human", fighter);
+  describe('Classes added', function() {
+    describe('Fighter', function() {
+      let fighter = new Fighter('skills');
+      let character = new Character("Ryan", "Human", fighter);
 
-  it('should load Name, Race, and fighter', function() {
-    expect(character.name).to.eql('Ryan');
-    expect(character.race).to.eql('Human');
-    expect(character.cClass).to.be.instanceOf(Fighter);
-  })
+      it('should load Name, Race, and fighter', function() {
+        expect(character.name).to.eql('Ryan');
+        expect(character.race).to.eql('Human');
+        expect(character.cClass).to.be.instanceOf(Fighter);
+      })
 
-  it('should be able to load all of the fighter constructor', function() {
-    expect(character.cClass.name).to.eql('Fighter');
-    expect(character.cClass.bonus).to.eql( { str: 1, dex: 1, const:1 } );
+      it('should be able to load all of the fighter constructor', function() {
+        expect(character.cClass.name).to.eql('Fighter');
+        expect(character.cClass.bonus).to.eql( { str: 1, dex: 1, const:1 } );
+      })
+
+      it('should add bonus to character', function() {
+        expect(character.stats.str).to.eql(13)
+        expect(character.stats.dex).to.eql(13)
+        expect(character.stats.wis).to.eql(13)
+      })
+    })
   })
 })
