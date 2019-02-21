11f09154a6e1f83679d2539f4af88c3aacc71e75
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-21 13:34:22 +0000

    wrote test to assure subrace could be passed

--- cypress/integration/character/races/human_test.js
+++ cypress/integration/character/races/human_test.js
@@ -2,14 +2,19 @@ import Human from '../../../../models/characters/races/human.js';
 
 describe('Human', function() {
   let human = new Human();
+  let steelEye = new Human("Steel Eye")
 
   describe('Constructor', function() {
-    it('Should have the name Human', function() {
+    it('should have the name Human', function() {
       expect(human.name).to.eql('Human')
     })
 
     it('should have default subrace', function() {
       expect(human.subrace).to.eql("Standard")
+    })
+
+    it('should pass a subrace to human on init', function() {
+      expect(steelEye.subrace).to.eql("Steel Eye")
     })
   })
 })
