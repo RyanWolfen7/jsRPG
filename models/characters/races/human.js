1b458e12c403b8dca2f98ebab09282d6696a0cd6
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-21 13:38:29 +0000

    added statBonus to constructor on human

--- models/characters/races/human.js
+++ models/characters/races/human.js
@@ -1,11 +1,17 @@
 import Race from '../race.js';
 
+const STAT_BONUS = {
+  str: 0, dex: 0, const: 0,
+  char: 2, wis: 0, int: 2
+}
+
 class Human extends Race {
   constructor(subrace = "Standard") {
     super(subrace);
 
     this.name = 'Human',
-    this.subrace = subrace
+    this.subrace = subrace,
+    this.statBonus = STAT_BONUS
   }
 }
 
