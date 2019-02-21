450b5ee2881a9d4dad7431f5010c906117667c7c
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-21 13:43:29 +0000

    added core modifier test

--- cypress/integration/character/races/human_test.js
+++ cypress/integration/character/races/human_test.js
@@ -7,6 +7,10 @@ describe('Human', function() {
     str: 0, dex: 0, const: 0,
     char: 2, wis: 0, int: 2
   }
+  let coreModifiers = {
+    health: 0, magic: 0,
+    stamina: 0, xp: -20
+  }
 
   describe('Constructor', function() {
     it('should have the name Human', function() {
@@ -23,6 +27,10 @@ describe('Human', function() {
 
     it('should have the right stat bonus', function() {
       expect(human.statBonus).to.eql(statBonus)
+    })
+
+    it('should have the right modifiers', function() {
+      expect(human.coreModifiers).to.eql(coreModifiers)
     })
   })
 })
